8ab68b16aedc63edf6659b8e7395f9f080b125de
saviily <sawa_83@mail.ru>
2014-11-26 21:49:31 +0300

    Задача SubModules

---
+++ lecture-6/SubModules.py
@@ -0,0 +1,9 @@
+mod = raw_input()
+
+try:
+    module = __import__(mod)
+    dirs = dir(module)
+    counter = [m for m in dirs if type(getattr(module, m)) == type(module)]
+    print len(counter)
+except ImportError:
+    print -1
