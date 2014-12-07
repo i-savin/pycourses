70f01949b18cea62ccb50fec415762731230bc5d
saviily <sawa_83@mail.ru>
2014-12-07 21:04:26 +0300

    Vogel fields

--- lecture-8/FakeField.py
+++ lecture-8/FakeField.py
@@ -8,3 +8,19 @@
 # объекта должно быть естественным (вызывть исключение AttributeError, 
 # как минимум с тем же сообщением, что и «естественный» AttributeError 
 # в случае несуществующего поля). Реализовывать что-то, кроме получения значения поля, не надо.
+
+class Vovel:
+    def __getattr__(self, item):
+        vovels = set('aeiouy');
+        item_set = set(str(item));
+        if item_set.difference(vovels):
+            raise AttributeError("Vovel instance has no attribute '" + str(item) + "'");
+        else:
+            return str(item).upper();
+
+# A = Vovel()
+# print A.aoao
+# try:
+#   print A.field
+# except AttributeError, msg:
+#   print "ERROR",msg
